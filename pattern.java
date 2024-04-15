b2169f4fe963f59270569ac0f42d9753a76774c0
Abhishek10075 <aknishad6677@gmail.com>
2024-04-15 16:44:10 +0530

    first commit

---
+++ pattern.java
@@ -0,0 +1,60 @@
+class pattern
+{
+
+public static void main(String[] args)
+{
+  
+/*for(int i=1;i<=3;i++)
+{
+   for(int j=1;j<=i;j++)
+{
+   System.out.print(" *");
+}
+System.out.println("");
+}
+System.out.println("SECONT PATTERNS");
+
+// ***
+// **
+// *
+  for(int i=1;i<=3;i++)
+{
+   for(int j=3;j>=i;j--)
+{
+   System.out.print(" *");
+}
+System.out.println("");
+}
+
+System.out.println("Third pattern ");
+for(int i=1;i<=5;i++)
+{
+  for(int j=i;j<5;j++)
+{
+  System.out.print(" ");
+}
+for(int k=1;k<i;k++)
+{
+ System.out.println("*");
+}
+System.out.println("");
+}
+*/
+System.out.println("fourth code");
+
+for(int i=1;i<=5;i++)
+{
+  for(int j=1;j<i;j++)
+{
+  System.out.print(" ");
+}
+for(int k=i;k<5;k++)
+{
+ System.out.print("*");
+}
+System.out.println("");
+}
+
+
+}
+}
